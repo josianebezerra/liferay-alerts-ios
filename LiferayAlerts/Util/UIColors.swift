77925d6c5ed311f42d58954accf86cc14cd41f74
Silvio Santos <silvio.santos@liferay.com>
2014-12-01 17:21:24 -0300

    Comment table view cell initial layout

--- LiferayAlerts/Util/UIColors.swift
+++ LiferayAlerts/Util/UIColors.swift
@@ -19,12 +19,17 @@ import UIKit
  */
 struct UIColors {
 
+	static let DARK_GRAY: UIColor = UIColor(rgb: "233240")
+	static let DARK_GRAY_ALPHA_40: UIColor = UIColor(rgb: "26233240")
 	static let WHITE: UIColor = UIColor(rgb:"FFFFFF")
 
 	static let CARD_BOTTOM_BAR_TEXT = UIColor(rgb: "66233240")
-	static let CARD_MESSAGE = UIColor(rgb: "233240")
+	static let CARD_MESSAGE = DARK_GRAY
 	static let CARD_SHAPE_BACKGROUND = WHITE
 	static let CARD_SHAPE_STROKE = UIColor(rgb: "D4D4D4")
+
+	static let COMMENT_TEXT = DARK_GRAY
+	static let COMMENT_DATE = DARK_GRAY_ALPHA_40
 
 	static let POLLS_CARD_CHOICE_BACKGROUND_1 = UIColor(rgb: "FFBF1F")
 	static let POLLS_CARD_CHOICE_BACKGROUND_2 = UIColor(rgb: "FCD435")
@@ -37,6 +42,6 @@ struct UIColors {
 	static let TOP_BAR_LAST_ALERT: UIColor = UIColor(rgb:"B29734")
 	static let TOP_BAR_USER_NAME: UIColor = WHITE
 	static let TOP_BAR_USER_NAME_SHADOW: UIColor = UIColor(rgb:"E4C143")
-	static let VERTICAL_LINE_COLOR: UIColor = UIColor(rgb: "26233240")
+	static let VERTICAL_LINE_COLOR: UIColor = DARK_GRAY_ALPHA_40
 
 }
